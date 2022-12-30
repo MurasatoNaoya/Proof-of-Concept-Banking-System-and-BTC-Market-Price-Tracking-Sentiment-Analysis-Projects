df551340cc170e19f9a29984dd8177e884b36c88
Andrew Naoya <ft22584@bristol.ac.uk>
2022-12-30 12:02:46 +0000

    Adding more methods to classes and main function. Some exception handling also added.

--- Courseworks/Part 1/main.py
+++ Courseworks/Part 1/main.py
@@ -1,22 +1,22 @@
 
 def main():
+
+    BankingSystemInstance = BankingSystem()
     while (True): 
-        BankingSystemInstance = BankingSystem(transaction_fees = 0, customers = {})
+
         BankingSystemInstance.main_display()
+
         while (True):
             decision = input("Enter choice: ")
 
-
-            if (int(decision)==1):
-            
+            if (int(decision)) == 1:
                 BankingSystemInstance.create_account()
                 
                 break
 
-            else: 
-                print('You didnt input 1!')
-                break 
-
+            elif (int(decision)) == 2: 
+                BankingSystemInstance.login_screen()
+                
 
 
 
@@ -40,6 +40,30 @@ class Customer():
         self.username = username 
 
 
+    def create_wallet(self, wallet_name, wallet_type):
+        """Create a new wallet with a given name and type."""
+        if wallet_name in self.wallets:
+            raise ValueError("A wallet with this name already exists")
+        # Create the appropriate type of wallet based on the provided type
+        if wallet_type == "daily_use":
+            self.wallets[wallet_name] = DailyUseWallet()
+        elif wallet_type == "savings":
+            self.wallets[wallet_name] = SavingsWallet()
+        elif wallet_type == "holidays":
+            self.wallets[wallet_name] = HolidaysWallet()
+        elif wallet_type == "mortgage":
+            self.wallets[wallet_name] = MortgageWallet()
+        else:
+            # None of the inputted names match a wallet type. 
+            # A ValueError is returned until a valid wallet name is inputted. 
+            raise ValueError("Invalid wallet type")
+
+
+    def delete_wallet(self, wallet_name):
+            """Delete a wallet with a given name."""
+            if wallet_name not in self.wallets:
+                raise ValueError("A wallet with this name does not exist")
+            del self.wallets[wallet_name]
 
 
 
@@ -47,13 +71,23 @@ class BankingSystem():
 
     
     
-    def __init__(self, transaction_fees, customers):
+    def __init__(self):
 
-        self.transaction_fees = transaction_fees
-        self.customers = customers
+        self.transaction_fees = 0 # Transaction fees start at zero for every run of the program. 
+        self.customers = {} # Empty dictionary to store customer accounts. 
 
 
-    def main_display(self):  # Displaying the initial text-based screen, where the customer will see their inital options. 
+    def login(self, username, password):
+        """Verify the provided credentials and return the customer object for the logged in user."""
+        if username not in self.customers:
+            raise ValueError("An account with this username does not exist")
+        if self.customers[username].password != password:
+            raise ValueError("Incorrect password")
+        return self.customers[username]
+
+
+
+    def main_display(self):  # Displaying the text-based screen, where the customer will see their inital options. 
 
         print("""
                 ======= UOB Banking System =======
@@ -62,6 +96,47 @@ class BankingSystem():
                   
                   or enter any other key to exit
                   """)
+
+
+    def login_screen():
+        while True:
+            # Print the menu options
+            print("\nMenu Options:")
+            print("1. Login")
+            print("2. Quit")
+
+            # Get the user's choice
+            choice = int(input("Enter your choice: "))
+
+            # Perform the appropriate action based on the user's choice
+            if choice == 1:
+                # Get the input for the login credentials
+                username = input("Enter your username: ")
+                password = input("Enter your password: ")
+
+                # Attempt to login with the provided credentials
+                try:
+                    customer = BankingSystem.login(username, password)
+                except ValueError as e:
+                    # Print an error message if the login fails
+                    print(e)
+                else:
+                    # If the login is successful, break out of the loop and return the customer object
+                    print("Login successful")
+                    break
+            elif choice == 2:
+                # Break the while loop and return to the main page. 
+                print("Exiting login screen and returning to the main page..")
+                break
+            else:
+                # Print an error message if an invalid choice is entered
+                print("Invalid choice. Please try again.")
+
+        return customer
+
+
+
+
 
 
     def create_account(self):
@@ -86,6 +161,64 @@ class BankingSystem():
 
 
 
+
+
+
+
+
+class Wallet:
+    def __init__(self):
+        self.balance = 0  # Initial balance is 0
+
+    # All of the wallets have a deposit method, so this can be inherited for all variations. 
+    def deposit(self, amount):
+        """Deposit a given amount of money into the wallet."""
+        self.balance += amount
+        
+
+
+class DailyUseWallet(Wallet):
+    def withdraw(self, amount):
+        """Withdraw a given amount of money from the wallet."""
+        if amount > self.balance:
+            raise ValueError("Insufficient balance")
+        self.balance -= amount
+
+    def transfer(self, amount, other_wallet):
+        """Transfer a given amount of money to another wallet."""
+        self.withdraw(amount)
+        other_wallet.deposit(amount)
+
+    def transfer_to_customer(self, amount, customer, customer_wallet_name):
+        """Transfer a given amount of money to another customer's wallet."""
+        self.withdraw(amount)
+        customer.wallets[customer_wallet_name].deposit(amount)
+
+
+
+class SavingsWallet(Wallet):
+    def withdraw(self, amount):
+        """Withdraw a given amount of money from the wallet."""
+        if amount > self.balance:
+            raise ValueError("Insufficient balance")
+        self.balance -= amount
+
+
+
+class HolidaysWallet(DailyUseWallet):
+    
+    # Holiday wallets have all the same functionality as Daily Use Wallets. 
+    # Meaning a error can be raised for the unwanted method, and it can be considered a Holiday wallet. 
+    def transfer_to_customer(self, amount, recipient, wallet_name):
+        """Transfer a given amount of money to another customer's wallet."""
+        raise ValueError("Holidays wallets cannot be used to transfer funds to other customers")
+
+
+
+class MortgageWallet(Wallet):
+    # The mortgage wallet has the same functionality as the base Wallet class. 
+    # So no additional functions need to be inherited. 
+    pass 
 
 
 
