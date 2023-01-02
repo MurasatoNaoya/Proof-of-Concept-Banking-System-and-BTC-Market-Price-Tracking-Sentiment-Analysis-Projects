b8b0970fe43b7259fe4f291800a299b50d4fb976
Andrew Naoya <ft22584@bristol.ac.uk>
2023-01-02 23:07:30 +0000

    Added docstrings and additional comments to methods of the Customer class.

--- Courseworks/Part 1/Customer.py
+++ Courseworks/Part 1/Customer.py
@@ -1,12 +1,11 @@
 # Customer class. 
-# To be later imoported into the main file.  
+# To be later imported into the main file.  
 
-
-from Wallets import *
+from Wallets import * # Importing all wallet classes associated with the file 'Wallets'.
 
 class Customer(): 
 
-    # Defining a constructor that instantiaties consumer objects. s
+    # Defining a constructor that instantiates consumer objectss. 
     def __init__(self, first_name, last_name, COR, age, email, username, password): 
         self.first_name = first_name
         self.last_name = last_name
@@ -21,12 +20,12 @@ class Customer():
         self.wallet_valid = [1, 2, 3, 4]
 
 
-
     def create_wallet(self):
-
+        """
+        Create a new wallet with a given name and wallet type.
+        """
         
-        """Create a new wallet with a given name and type."""
-        
+        # Additional textual interface to make the program more intuitive. 
         print("\n========= Wallet creation menu ==========")
         print(f'Welcome to the wallet creation menue, user {self.username}.')
         print('Please select one of the options to create a wallet for your specific needs.')
@@ -37,7 +36,8 @@ class Customer():
         print("3. Holidays ")
         print("4. Mortgage ") 
         print(' ')
-    
+
+        # Set up while loop to ensure a valid input in entered. 
         while True:
             try:
                 wallet_type = input('Please select the type of wallet you would like by entering one of the numbers above: ')
@@ -53,23 +53,24 @@ class Customer():
                 print("An invalid input has been entered, please try again or return to the account menu.")
                 print(' ')
 
-
         while True:
             wallet_name = input('Please enter a name for your wallet: ').rstrip()
             
+            # Check if a wallet that is associated with the account, of the same name already exists. 
+            # If such a wallet does exist, then the user will be prompted to enter another name. 
             if wallet_name in self.wallets: 
                 print(' ')
                 print("A wallet with this name already exists, please try a different name.")
                 print(' ')
 
             else: 
-                break
+                break # There is no other wallet with the same name, so the input is accepted. 
         
         self.wallet_id += 1
         wallet_id = f"{self.wallet_id:03d}"  # Format the wallet_id as a 3-digit string (e.g. 001, 002, etc.)
         # Create the appropriate type of wallet based on the provided type
 
-
+        # Daily wallet selection.
         if wallet_type == 1:
             self.wallets[wallet_name] = DailyUseWallet(wallet_id, wallet_name) # wallet_type is also defined as a static attribute.
             print(' ')
@@ -80,7 +81,7 @@ class Customer():
             print(' ')
             #print(self.wallets[wallet_name].wallet_type)
 
-
+        # Savings wallet selection. 
         elif wallet_type == 2:
             self.wallets[wallet_name] = SavingsWallet(wallet_id, wallet_name) # wallet_type is also defined as a static attribute.
             print(' ')
@@ -91,7 +92,7 @@ class Customer():
             print(' ')
             #print(self.wallets[wallet_name].wallet_type)
 
-
+        # Holidays wallet selection.
         elif wallet_type == 3:
             self.wallets[wallet_name] = HolidaysWallet(wallet_id, wallet_name) # wallet_type is also defined as a static attribute. 
             print(' ')
@@ -102,8 +103,7 @@ class Customer():
             print(' ')
             #print(self.wallets[wallet_name].wallet_type)
 
-
-                
+        # Mortgage wallet selection. 
         elif wallet_type == 4:
             self.wallets[wallet_name] = MortgageWallet(wallet_id, wallet_name) # wallet_type is also defined as a static attribute. 
             print(' ')
@@ -117,8 +117,13 @@ class Customer():
 
     
     def select_wallet(self):
+        """
+        Display all available wallets and allows the user to select one by number.
+        This function is used extensively for actions involved with wallets, like 
+        depositing, withdrawing and transferring.
+        """
 
-        """Display all available wallets and allow the user to select one by number."""
+        # Additional textual interface to make the program more intuitive. 
         print("\n========= Wallet selection menu ==========")
         print(f'Welcome to the wallet selection menu, user {self.username}.')
         print('Please select one of the available wallets by entering its number.')
@@ -146,10 +151,13 @@ class Customer():
 
 
     def display_wallets(self): 
-        
-        """Display all available wallets """
+        """
+        Display all available wallets. 
+        Users can press any key to exit the display screen.
+        """
         print(f"\n========= All available wallets for user: {self.username} ==========")
         print(' ')
+        # Iterate through all wallets associated with the customer account and print the information of interest. 
         for i, (name, wallet) in enumerate(self.wallets.items()):
             print(f'{i+1}. ID: {wallet.wallet_id}, Name: {wallet.wallet_name}, Wallet Type: "{wallet.wallet_type}", Balance: {wallet.balance}, Nature of last transaction: {wallet.last_transaction}')
         print(' ')
@@ -159,9 +167,12 @@ class Customer():
             return
 
 
-
     def delete_wallet(self): 
+        '''
+        Deletes a selected wallet associated with the customer account.
+        '''
         
+        # The select_wallet() method is implemented in order to specify the wallet the user wants to delete.≈
         wallet = self.select_wallet()
         
 
@@ -177,7 +188,7 @@ class Customer():
                 print(' ')
                 print("The entered value is not valid, please try again ans answer with either y, or n.")
 
-
+            # User confirms they want to delete the specified wallet. 
             elif confirm.lower() == 'y':
             
                 del(self.wallets[wallet.wallet_name])   
@@ -186,7 +197,7 @@ class Customer():
 
                 return
 
-            
+            # User rejects / changes their mind about deleting the specified wallet. 
             elif confirm.lower() == 'n':
                 
                 print(' ')
@@ -196,8 +207,7 @@ class Customer():
 
             else:
                 # When a value other than Y/y or N/n is inputted.
+                # As this is within a while loop, the user will be prompted for a valid input until they do. 
                 print(' ')
                 print("The entered value is not valid, please try again ans answer with either y, or n.")
-
-
 
